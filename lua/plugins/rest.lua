a3b3109646ee858f39fe1651bc8beeb2f08208f9
dysksh <dysksh8@gmail.com>
2026-03-08 23:01:46 +0900

    WSL上のコンテナ内作業のための変更

--- lua/plugins/rest.lua
+++ lua/plugins/rest.lua
@@ -1,47 +1,17 @@
 return {
   {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    config = function()
-      require('nvim-treesitter').setup {
-        ensure_installed = { 'http', 'json' },
-      }
-    end,
-  },
-  {
-    'rest-nvim/rest.nvim',
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter',
-      'nvim-lua/plenary.nvim',
-      { 'nvim-neotest/nvim-nio', name = 'nvim-nio' },
-      { 'j-hui/fidget.nvim', opts = {} },
-    },
+    'mistweaverco/kulala.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
     ft = 'http',
-    config = function()
-      ---@type rest.Opts
-      vim.g.rest_nvim = {
-        request = {
-          skip_ssl_verification = true,
-        },
-        response = {
-          hooks = {
-            decode_url = true,
-            format = true,
-          },
-        },
-      }
+    opts = {
+      global_keymaps = false,
+    },
+    config = function(_, opts)
+      require('kulala').setup(opts)
 
-      -- レスポンスの JSON を jq でフォーマットする
-      vim.api.nvim_create_autocmd('FileType', {
-        pattern = 'json',
-        callback = function()
-          vim.opt_local.formatprg = 'jq .'
-        end,
-      })
-      -- キーマッピング
-      vim.keymap.set('n', '<leader>rr', '<cmd>Rest run<CR>', { desc = 'Run REST request' })
-      vim.keymap.set('n', '<leader>rl', '<cmd>Rest last<CR>', { desc = 'Re-run last REST request' })
-      vim.keymap.set('n', '<leader>ro', '<cmd>Rest open<CR>', { desc = 'Open REST result pane' })
+      vim.keymap.set('n', '<leader>rr', '<cmd>lua require("kulala").run()<CR>', { desc = 'Run REST request' })
+      vim.keymap.set('n', '<leader>rl', '<cmd>lua require("kulala").replay()<CR>', { desc = 'Re-run last REST request' })
+      vim.keymap.set('n', '<leader>ro', '<cmd>lua require("kulala").toggle_view()<CR>', { desc = 'Toggle REST result view' })
     end,
   },
 }
